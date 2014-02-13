85e95a46e058d865eae77ce01b7dae9a28772c89
Dimitar Dimitrov <dimitar.dimitrov@students.olin.edu>
2014-02-13 10:04:27 -0500

    Gene Finder Code

--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Feb  2 11:24:42 2014
+Created on Wed Feb 12 2014
 
-@author: YOUR NAME HERE
+@author: Dimitar Dimitrov (dimitdim)
 """
 
-# you may find it useful to import these variables (although you are not required to use them)
 from amino_acids import aa, codons
+from random import shuffle
 
 def collapse(L):
     """ Converts a list of strings to a string by concatenating all elements of the list """
@@ -25,13 +25,21 @@ def coding_strand_to_AA(dna):
         returns: a string containing the sequence of amino acids encoded by the
                  the input DNA fragment
     """
-    
-    # YOUR IMPLEMENTATION HERE
+
+    amino=""
+    for i in range(len(dna)//3):
+        amino += {codon:aa[codons.index(codonn)] for codonn in codons for codon in codonn}[dna[i*3:(i+1)*3]]
+    return amino
+
 
 def coding_strand_to_AA_unit_tests():
     """ Unit tests for the coding_strand_to_AA function """
-        
-    # YOUR IMPLEMENTATION HERE
+
+    nuc=["CACGACAGAGGAC","GGCTGTTCTGGTTGC","AGTTCCGGCGGACCCCGT"]
+    ami=["HDRG","GCSGC","SSGGPR"]
+    for n in range(len(nuc)):
+        print "input: " + nuc[n] + ",\nexpected output: " + ami[n] + ",\nactual output: " + coding_strand_to_AA(nuc[n])
+
 
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
@@ -40,13 +48,30 @@ def get_reverse_complement(dna):
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
     """
-    
-    # YOUR IMPLEMENTATION HERE
-    
+
+    reverse=""
+    m=""
+    for n in dna:
+        if n in ('A', 'a'):
+            m = 'T'
+        elif n in ('T', 't'):
+            m = 'A'
+        elif n in ('G', 'g'):
+            m = 'C'
+        elif n in ('C', 'c'):
+            m = 'G'
+        reverse = m + reverse
+    return reverse
+
+
 def get_reverse_complement_unit_tests():
     """ Unit tests for the get_complement function """
-        
-    # YOUR IMPLEMENTATION HERE    
+
+    nuc=["CACGACAGAGGAC","GGCTGTTCTGGTTGC","AGTTCCGGCGGACCCCGT"]
+    rev=["GTCCTCTGTCGTG","GCAACCAGAACAGCC","ACGGGGTCCGCCGGAACT"]
+    for n in range(len(nuc)):
+        print "input: " + nuc[n] + ",\nexpected output: " + rev[n] + ",\nactual output: " +  get_reverse_complement(nuc[n])
+
 
 def rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start codon and returns
@@ -56,14 +81,24 @@ def rest_of_ORF(dna):
         dna: a DNA sequence
         returns: the open reading frame represented as a string
     """
-    
-    # YOUR IMPLEMENTATION HERE
+
+    stop=len(dna) 
+    for i in range(len(dna)//3):
+        if dna[i*3:(i+1)*3] in codons[10]:
+            stop=i*3
+            break
+    return dna[0:stop]
+
 
 def rest_of_ORF_unit_tests():
     """ Unit tests for the rest_of_ORF function """
-        
-    # YOUR IMPLEMENTATION HERE
-        
+
+    nuc=["ATGACAGAGGACTAG","ATGTGTTAATCTGGTTGC","ATGTCCGGCGGACCCCGT"]
+    cut=["ATGACAGAGGAC","ATGTGT","ATGTCCGGCGGACCCCGT"]
+    for n in range(len(nuc)):
+        print "input: " + nuc[n] + ",\nexpected output: " + cut[n] + ",\nactual output: " +  rest_of_ORF(nuc[n])
+
+
 def find_all_ORFs_oneframe(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence and returns
         them as a list.  This function should only find ORFs that are in the default
@@ -74,13 +109,26 @@ def find_all_ORFs_oneframe(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
-     
-    # YOUR IMPLEMENTATION HERE        
-     
+
+    ORFs=[]
+    i=0
+    while i < len(dna)//3:
+        if dna[i*3:(i+1)*3] in codons[3]:
+            orf=rest_of_ORF(dna[i*3:len(dna)])
+            ORFs.append(orf)
+            i+=len(orf)/3
+        i+=1
+    return ORFs
+
+
 def find_all_ORFs_oneframe_unit_tests():
     """ Unit tests for the find_all_ORFs_oneframe function """
 
-    # YOUR IMPLEMENTATION HERE
+    nuc=["CACGACATGGGACGGCTGTTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGATTAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGCCGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTGTGAGAAGCCCACCTGGGCCAGTTGAAGTTCGAGGATTCGGTGACGGACGACCACTGGGACCGGCGGGTGGAGGGGCGGCTCAAGTGGGGACGCCACGTGCGGAGGGACCTGTTCAAGGACCGAAGACACTCGTGGCACGACTGGATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAATAAATTTCAGACTCACCCGCC"]
+    orf=[["ATGGGACGGCTGTTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGG","ATGCGGGTCCAATTCCCGGTGCCGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTG","ATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAA"]]
+    for n in range(len(nuc)):
+	print "input: " + str(nuc[n]) + ",\nexpected output: " + str(orf[n]) + ",\nactual   output: " +  str(find_all_ORFs_oneframe(nuc[n]))
+
 
 def find_all_ORFs(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence in all 3
@@ -91,13 +139,18 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
-     
-    # YOUR IMPLEMENTATION HERE
+
+    return find_all_ORFs_oneframe(dna) + find_all_ORFs_oneframe(dna[1:len(dna)]) + find_all_ORFs_oneframe(dna[2:len(dna)])
+
 
 def find_all_ORFs_unit_tests():
     """ Unit tests for the find_all_ORFs function """
-        
-    # YOUR IMPLEMENTATION HERE
+
+    nuc=["CACGACATGGGACGGCATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGATGAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGACGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTGTGAGAAGCCCACCTGGGCCAGTTGAAGTTCGAGGATTCGGTGACGGACGACCACTGGGACCGGCGGGTGGAGGGGCGGCTCAAGTGGGGACGCCACGTGCGGAGGGACCTGTTCAAGGACCGAAGACACTCGTGGCACGACTGGATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAATAAATTTCAGACTCACCCGCC"]
+    orf=[["ATGGGACGGCATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGG","ATGAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGACGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTG","ATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAA","ATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGA"]]
+    for n in range(len(nuc)):
+	print "input: " + str(nuc[n]) + ",\nexpected output: " + str(orf[n]) + ",\nactual   output: " +  str(find_all_ORFs(nuc[n]))
+
 
 def find_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
@@ -106,24 +159,34 @@ def find_all_ORFs_both_strands(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
-     
-    # YOUR IMPLEMENTATION HERE
+
+    return find_all_ORFs(dna) + find_all_ORFs(get_reverse_complement(dna))
+
 
 def find_all_ORFs_both_strands_unit_tests():
     """ Unit tests for the find_all_ORFs_both_strands function """
 
-    # YOUR IMPLEMENTATION HERE
+    nuc=["CACGACATGGGACGGCATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGATGAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGACGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTGTGAGAAGCCCACCTGGGCCAGTTCATGTTCGAGGATTCGGTGACGGACGACCACTGGGACCGGCGGGTGGAGGGGCGGCTCAAGTGGGGACGCCACGTGCGGAGGGACCTGTTCAAGGACCGAAGACACTCGTGGCACGACTGGATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAATAAATTTCAGACTCACCCGCC"]
+    orf=[["ATGGGACGGCATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGG","ATGAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGACGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTG","ATGTTCGAGGATTCGGTGACGGACGACCACTGGGACCGGCGGGTGGAGGGGCGGCTCAAGTGGGGACGCCACGTGCGGAGGGACCTGTTCAAGGACCGAAGACACTCGTGGCACGACTGGATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAA","ATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGA","ATGGCTCCGAGGTCGAATTGCCATAAACATCCAGTCGTGCCACGAGTGTCTTCGGTCCTTGAACAGGTCCCTCCGCACGTGGCGTCCCCACTTGAGCCGCCCCTCCACCCGCCGGTCCCAGTGGTCGTCCGTCACCGAATCCTCGAACATGAACTGGCCCAGGTGGGCTTCTCACACGCGCACGTCCAGCGAGTCCCGCCTGTCGCGCAACCCGTACAGCAGGTGCACGCGGTGCCGCAACCAGTCGCGCAGCCGGTGGAAGAACGTCACCGGGAATTGGACCCGCATCGGCACCGAGTCCAGCTTCACGCCCTTCATCCAGAACTACCACCCCTTCCTGTCCTTGTAGGAGAGGTCCCGGAGGCGTGGTATGAGCGGTCGCACGCGCGGTTGGAACGGGGTCCGCCGGAACTGCAACCAGACATGCCGTCCCATGTCGTG","ATGAACTGGCCCAGGTGGGCTTCTCACACGCGCACGTCCAGCGAGTCCCGCCTGTCGCGCAACCCGTACAGCAGGTGCACGCGGTGCCGCAACCAGTCGCGCAGCCGGTGGAAGAACGTCACCGGGAATTGGACCCGCATCGGCACCGAGTCCAGCTTCACGCCCTTCATCCAGAACTACCACCCCTTCCTGTCCTTG","ATGAGCGGTCGCACGCGCGGTTGGAACGGGGTCCGCCGGAACTGCAACCAGACATGCCGTCCCATGTCGTG"]]
+    for n in range(len(nuc)):
+	print "input: " + str(nuc[n]) + ",\nexpected output: " + str(orf[n]) + ",\nactual   output: " +  str(find_all_ORFs_both_strands(nuc[n]))
+
 
 def longest_ORF(dna):
     """ Finds the longest ORF on both strands of the specified DNA and returns it
         as a string"""
 
-    # YOUR IMPLEMENTATION HERE
+    return max(find_all_ORFs_both_strands(dna), key=len)
+
 
 def longest_ORF_unit_tests():
     """ Unit tests for the longest_ORF function """
 
-    # YOUR IMPLEMENTATION HERE
+    nuc=["CACGACATGGGACGGCATGTCTGGTTGCAGTTCCGGCGGACCCCGTTCCAACCGCGCGTGCGACCGCTCATACCACGCCTCCGGGACCTCTCCTACAAGGACAGGAAGGGGTGGTAGTTCTGGATGAAGGGCGTGAAGCTGGACTCGGTGCCGATGCGGGTCCAATTCCCGGTGACGTTCTTCCACCGGCTGCGCGACTGGTTGCGGCACCGCGTGCACCTGCTGTACGGGTTGCGCGACAGGCGGGACTCGCTGGACGTGCGCGTGTGAGAAGCCCACCTGGGCCAGTTCATGTTCGAGGATTCGGTGACGGACGACCACTGGGACCGGCGGGTGGAGGGGCGGCTCAAGTGGGGACGCCACGTGCGGAGGGACCTGTTCAAGGACCGAAGACACTCGTGGCACGACTGGATGTTTATGGCAATTCGACCTCGGAGCCATCGTCAAGGAGGACGGTCTACCCGGAGGGTTGCCCGGGAGGAGGGGAGGAACGTGGCCGGGAAGGACCAGAAATAAATTTCAGACTCACCCGCC"]
+    lon=["ATGGCTCCGAGGTCGAATTGCCATAAACATCCAGTCGTGCCACGAGTGTCTTCGGTCCTTGAACAGGTCCCTCCGCACGTGGCGTCCCCACTTGAGCCGCCCCTCCACCCGCCGGTCCCAGTGGTCGTCCGTCACCGAATCCTCGAACATGAACTGGCCCAGGTGGGCTTCTCACACGCGCACGTCCAGCGAGTCCCGCCTGTCGCGCAACCCGTACAGCAGGTGCACGCGGTGCCGCAACCAGTCGCGCAGCCGGTGGAAGAACGTCACCGGGAATTGGACCCGCATCGGCACCGAGTCCAGCTTCACGCCCTTCATCCAGAACTACCACCCCTTCCTGTCCTTGTAGGAGAGGTCCCGGAGGCGTGGTATGAGCGGTCGCACGCGCGGTTGGAACGGGGTCCGCCGGAACTGCAACCAGACATGCCGTCCCATGTCGTG"]
+    for n in range(len(nuc)):
+	print "input: " + str(nuc[n]) + ",\nexpected output: " + str(lon[n]) + ",\nactual   output: " +  str(longest_ORF(nuc[n]))
+
 
 def longest_ORF_noncoding(dna, num_trials):
     """ Computes the maximum length of the longest ORF over num_trials shuffles
@@ -133,7 +196,13 @@ def longest_ORF_noncoding(dna, num_trials):
         num_trials: the number of random shuffles
         returns: the maximum length longest ORF """
 
-    # YOUR IMPLEMENTATION HERE
+    l=0
+    for i in range(num_trials):
+        lst=list(dna)
+        shuffle(lst)
+        l=max(l,len(longest_ORF(collapse(lst))))
+    return l
+
 
 def gene_finder(dna, threshold):
     """ Returns the amino acid sequences coded by all genes that have an ORF
@@ -146,4 +215,9 @@ def gene_finder(dna, threshold):
                  length specified.
     """
 
-    # YOUR IMPLEMENTATION HERE
+    orflst=find_all_ORFs_both_strands(dna)
+    lon=[]
+    for orf in orflst:
+        if len(orf) > threshold:
+            lon.append(coding_strand_to_AA(orf))
+    return lon
